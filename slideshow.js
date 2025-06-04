4bdc308d7f8cd664d2a3fdd1b636aca83d854287
website <161450854+MaryPadiz25@users.noreply.github.com>
2025-06-04 08:53:42 +0800

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -1,14 +1,16 @@
+
 function showSlides(containerClass) {
     let index = 0;
     const slides = document.querySelectorAll(`.mySlides.${containerClass}`);
-    setInterval(() => {
-        slides.forEach((slide) => (slide.style.display = "none"));
-        index++;
-        if (index > slides.length) {index = 1}
-        slides[index - 1].style.display = "block";
-    }, 2000); 
-}
+    slides.forEach(slide => slide.style.display = "block"); // Ensure initial visibility
 
+    setInterval(() => {
+        slides.forEach(slide => slide.style.display = "none");
+        index++;
+        if (index >= slides.length) { index = 0; }
+        slides[index].style.display = "block";
+    }, 2000);
+}
 showSlides('container1');
 showSlides('container2');
 showSlides('container3');
